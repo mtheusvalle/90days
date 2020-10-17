d92df6dfc895002c0ae385f5bd178e6f4f376c6b
mtheusvalle <55810334+mtheusvalle@users.noreply.github.com>
2020-10-17 00:06:57 -0300

    05

--- 05rps/scripts.js
+++ 05rps/scripts.js
@@ -8,51 +8,68 @@ function play(select) {
     selectPlayer = select;
     selectComputer = Math.floor(Math.random() * (2 + 1) + 1);
 
+    console.log(selectComputer);
+
     $('#rock').removeClass('winner');
     $('#paper').removeClass('winner');
     $('#scissors').removeClass('winner');
 
     if( selectPlayer == selectComputer ) {
-        console.log('Empate');
         $('#messageText').text('Empate');
     }
-    
+
+    switch(selectComputer){
+        case 1:
+            function rock() {
+                $('#rock').addClass('winner');
+            };
+            rock();
+            break;
+        case 2:
+            function paper() {
+                $('#paper').addClass('winner');
+            };
+            paper()
+            break;
+        case 3:
+            function scissors() {
+                $('#scissors').addClass('winner');
+            };
+            scissors()
+            break;
+    }
+
     // Humano Ganhar
     if ( selectPlayer == 1 && selectComputer == 3 ) {
         scoreHuman++;
-        $('#scoreHuman').html('<p>' + scoreHuman + '</p>');
-        $('#scissors').addClass('winner');
+        $('.humanScore p').html('<p>' + scoreHuman + '</p>');
         $('#messageText').text('Você ganhou!');
     } 
     else if ( selectPlayer == 2 && selectComputer == 1 ) {
         scoreHuman++;
-        $('#scoreHuman').html('<p>' + scoreHuman + '</p>');
-        $('#rock').addClass('winner');
+        $('.humanScore p').html('<p>' + scoreHuman + '</p>');
         $('#messageText').text('Você ganhou!');
     } 
     else if ( selectPlayer == 3 && selectComputer == 2 ) {
         scoreHuman++;
-        $('#scoreHuman').html('<p>' + scoreHuman + '</p>');
-        $('#paper').addClass('winner');
+        $('.humanScore p').html('<p>' + scoreHuman + '</p>');
         $('#messageText').text('Você ganhou!');
     }
       // Computador Ganhar
     else if ( selectPlayer == 1 && selectComputer == 2 ) {
         scoreComputer++;
-        $('#scissors').addClass('winner');
-        $('#scoreComputer').html('<p>' + scoreComputer + '</p>');
+        $('.computerScore p').html('<p>' + scoreComputer + '</p>');
         $('#messageText').text('Computador ganhou!');
     } 
     else if ( selectPlayer == 2 && selectComputer == 3 ) {
         scoreComputer++;
-        $('#paper').addClass('winner');
-        $('#scoreComputer').html('<p>' + scoreComputer + '</p>');
+        $('.computerScore p').html('<p>' + scoreComputer + '</p>');
         $('#messageText').text('Computador ganhou!');
     } 
     else if (selectPlayer == 3 && selectComputer == 1 ) {
         scoreComputer++;
-        $('#rock').addClass('winner');
-        $('#scoreComputer').html('<p>' + scoreComputer + '</p>');
+        $('.computerScore p').html('<p>' + scoreComputer + '</p>');
         $('#messageText').text('Computador ganhou!');
     }
+
 }
